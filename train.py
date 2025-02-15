b5ddca5e03ad16561e75b58a0bde534b0568bb85
Saksham Goel <saksham.goel29@gmail.com>
2025-02-15 08:03:35 -0500

    init for train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+"""
+Saksham's Custom Training Implementation, will be used for training the Tic Tac Toe AI.
+The AIs will play against each other and learn from their mistakes, no data collection, just play!
+not sure if this will work, but its worth a try!
+
+Author: Saksham Goel
+Date: Feb 15, 2025
+Version: 1.0
+
+Github: @SakshamG7
+Organization: AceIQ
+Website: https://aceiq.ca
+Contact: mail@aceiq.ca
+Location: Canada, ON, Oakville
+"""
