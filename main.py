58dccd49521eb055a00c4d53f0b4c80e64a24797
Saksham Goel <saksham.goel29@gmail.com>
2025-02-15 08:21:29 -0500

    Add Tic Tac Toe game implementation and enhance neural network with fitness, crossover, and mutation methods

--- main.py
+++ main.py
@@ -17,3 +17,82 @@ Location: Canada, ON, Oakville
 # Required Libraries
 import snn
 
+# Tic Tac Toe Game
+class TicTacToe(object):
+    # __init__: constructor for the Tic Tac Toe game
+    def __init__(self) -> None:
+        self.board = [0] * 9
+        self.turn = 1
+        self.winner = 0
+        self.over = False
+    
+    def reset(self) -> None:
+        self.board = [0] * 9
+        self.turn = 1
+        self.winner = 0
+        self.over = False
+
+    # play: plays a move on the board
+    # move -> int: the move to play
+    def play(self, move: int) -> None:
+        if self.over:
+            return
+        
+        if self.board[move] == 0:
+            self.board[move] = self.turn
+            self.turn = -self.turn
+        else:
+            return
+
+        self.check_winner()
+        self.check_draw()
+
+    def print_board(self) -> None:
+        for i in range(3):
+            print(self.board[3 * i:3 * i + 3])
+
+    # check_winner: checks if there is a winner
+    def check_winner(self) -> bool:
+        for i in range(3):
+            if self.board[i] == self.board[i + 3] == self.board[i + 6] != 0:
+                self.winner = self.board[i]
+                self.over = True
+                return True
+            if self.board[3 * i] == self.board[3 * i + 1] == self.board[3 * i + 2] != 0:
+                self.winner = self.board[3 * i]
+                self.over = True
+                return True
+        if self.board[0] == self.board[4] == self.board[8] != 0:
+            self.winner = self.board[0]
+            self.over = True
+            return True
+        if self.board[2] == self.board[4] == self.board[6] != 0:
+            self.winner = self.board[2]
+            self.over = True
+            return True
+        if all([x != 0 for x in self.board]):
+            self.over = True
+            return True
+        return False
+
+    # check_draw: checks if the game is a draw
+    def check_draw(self) -> bool:
+        if all([x != 0 for x in self.board]):
+            self.over = True
+            return True
+        return False
+    
+
+# Training the AI
+
+# Hyperparameters, most numbers are arbitrary
+population_size = 100
+generations = 1000
+mutation_rate = 0.05
+elite_percentage = 0.1 # The top 10% of the population will be carried over to the next generation, ensures that the best AI is not lost
+
+# Parameters
+input_size = 9
+hidden_layers = 3 # Some arbitrary number I chose
+hidden_size = [9, 3, 9] # Some arbitrary numbers I chose, resembles a bottleneck architecture
+output_size = 9
--- snn.py
+++ snn.py
@@ -15,6 +15,7 @@ Location: Canada, ON, Oakville
 """
 
 # Required Libraries
+from __future__ import annotations # Required for type hinting the class itself
 import gpy
 import math
 import random
@@ -43,20 +44,30 @@ class SimpleNeuralNetwork(object):
         self.output_size = output_size
         self.hidden_layers = hidden_layers
         self.hidden_size = hidden_size
+        self.fitness = 0
         # Initialize the weights and biases with random values
         self.weights = []
         self.biases = []
 
         for i in range(hidden_layers + 1):
             if i == 0:
-                self.weights.append(gpy.matrix([[random.random() for j in range(hidden_size[i])] for k in range(input_size)]))
-                self.biases.append(gpy.matrix([[random.random()] for j in range(hidden_size[i])]))
+                self.weights.append(gpy.matrix([[random.uniform(-1, 1) for j in range(hidden_size[i])] for k in range(input_size)]))
+                self.biases.append(gpy.matrix([[random.uniform(-1, 1)] for j in range(hidden_size[i])]))
             elif i == hidden_layers:
-                self.weights.append(gpy.matrix([[random.random() for j in range(output_size)] for k in range(hidden_size[i - 1])]))
-                self.biases.append(gpy.matrix([[random.random()] for j in range(output_size)]))
+                self.weights.append(gpy.matrix([[random.uniform(-1, 1) for j in range(output_size)] for k in range(hidden_size[i - 1])]))
+                self.biases.append(gpy.matrix([[random.uniform(-1, 1)] for j in range(output_size)]))
             else:
-                self.weights.append(gpy.matrix([[random.random() for j in range(hidden_size[i])] for k in range(hidden_size[i - 1])]))
-                self.biases.append(gpy.matrix([[random.random()] for j in range(hidden_size[i])]))
+                self.weights.append(gpy.matrix([[random.uniform(-1, 1) for j in range(hidden_size[i])] for k in range(hidden_size[i - 1])]))
+                self.biases.append(gpy.matrix([[random.uniform(-1, 1)] for j in range(hidden_size[i])]))
+
+    # Set the fitness of the neural network
+    def set_fitness(self, fitness: float) -> None:
+        self.fitness = fitness
+
+    # get_fitness: returns the fitness of the neural network
+    # returns -> float: the fitness of the neural network
+    def get_fitness(self) -> float:
+        return self.fitness
 
     # forward: forward pass of the neural network
     # x -> list: the input list
@@ -79,3 +90,47 @@ class SimpleNeuralNetwork(object):
                 final_output = final_output.apply(LeakyReLU)
 
         return softmax(final_output) # Apply the softmax function to get the probabilities
+
+    # copy: returns a copy of the neural network
+    # returns -> SimpleNeuralNetwork: the copy of the neural network
+    def copy(self) -> SimpleNeuralNetwork:
+        new_nn = SimpleNeuralNetwork(self.input_size, self.hidden_layers, self.hidden_size, self.output_size)
+        new_nn.weights = [x.copy() for x in self.weights]
+        new_nn.biases = [x.copy() for x in self.biases]
+        return new_nn
+
+    # crossover: crossover function for the neural network
+    # partner -> SimpleNeuralNetwork: the partner neural network
+    # returns -> SimpleNeuralNetwork: the child neural network
+    def crossover(self, partner: SimpleNeuralNetwork) -> SimpleNeuralNetwork:
+        # Very simple crossover, just a 50-50 split, and we know the structure of the neural network is the same, so no Augmenting Topologies sadly
+        child = self.copy()
+        # Crossover the weights
+        for i in range(len(self.weights)):
+            for j in range(self.weights[i].rows):
+                for k in range(self.weights[i].cols):
+                    if random.uniform(-1, 1) < 0.5:
+                        child.weights[i].data[j][k] = partner.weights[i].data[j][k]
+
+        # Crossover the biases
+        for i in range(len(self.biases)):
+            for j in range(self.biases[i].rows):
+                    if random.uniform(-1, 1) < 0.5:
+                        child.biases[i].data[j][0] = partner.biases[i].data[j][0] # shape is (rows, 1)
+
+        return child
+    
+    # mutate: mutation function for the neural network
+    # mutation_rate -> float: the rate of mutation
+    def mutate(self, mutation_rate: float) -> None:
+        # Mutate the weights
+        for i in range(len(self.weights)):
+            for j in range(self.weights[i].rows):
+                for k in range(self.weights[i].cols):
+                    if random.uniform(-1, 1) < mutation_rate:
+                        self.weights[i].data[j][k] += random.uniform(-1, 1)
+
+        for i in range(len(self.biases)):
+            for j in range(self.biases[i].rows):
+                    if random.uniform(-1, 1) < mutation_rate:
+                        self.biases[i].data[j][0] += random.uniform(-1, 1)
