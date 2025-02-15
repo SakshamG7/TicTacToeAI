8f37ec92f176a013ddfe4b3f3c6add1152f0c98d
Saksham Goel <saksham.goel29@gmail.com>
2025-02-15 07:24:59 -0500

    Copied over my gpy implemention from my previous project. Started on my snn.py neural network implementation

---
+++ gpy.py
@@ -0,0 +1,138 @@
+"""
+Saksham's Custom Matrix Implementation (Goel's Python)
+
+Author: Saksham Goel
+Date: Feb 12, 2025
+Version: 1.0
+
+Github: @SakshamG7
+Organization: AceIQ
+Website: https://aceiq.ca
+Contact: mail@aceiq.ca
+Location: 
+"""
+
+from __future__ import annotations
+
+### matrix class
+# rows: the number of rows in the matrix
+# cols: the number of columns in the matrix
+# data: the data of the matrix
+class matrix(object):
+    # __init__: the constructor for the matrix class
+    def __init__(self, data: list = [], rows: int = -1, cols: int = -1) -> None:
+        self.rows = rows
+        self.cols = cols
+        self.data = data
+        if len(data) == 0 and rows == -1 and cols == -1:
+            raise ValueError("No data or dimensions provided")
+
+        if len(data) != 0:
+            self.rows = len(data)
+            self.cols = len(data[0])
+        if rows != -1 and cols != -1:
+            self.data = [[0 for i in range(cols)] for j in range(rows)]
+    
+    # transpose: transposes the matrix itself
+    def transpose(self) -> None:
+        new_data = [[0 for i in range(self.rows)] for j in range(self.cols)]
+        for i in range(self.rows):
+            for j in range(self.cols):
+                new_data[j][i] = self.data[i][j]
+        
+        self.data = new_data
+        self.rows, self.cols = self.cols, self.rows
+    
+    # copy: returns a copy of the matrix
+    def copy(self) -> matrix:
+        return matrix(self.data.copy())
+    
+    # __add__: adds two matrices together
+    def __add__(self, other: matrix) -> matrix:
+        if self.rows != other.rows or self.cols != other.cols:
+            return None
+        result = matrix(self.data.copy())
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[i][j] += other.data[i][j]
+        return result
+    
+    # __sub__: subtracts two matrices
+    def __sub__(self, other: matrix) -> matrix:
+        if self.rows != other.rows or self.cols != other.cols:
+            return None
+        result = matrix(self.data.copy())
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[i][j] -= other.data[i][j]
+        return result
+    
+    # __mul__: multiplies two matrices, returns the dot product
+    def __mul__(self, other: matrix) -> matrix:
+        if self.cols != other.rows:
+            return None
+        result = matrix(len(self), len(other[0]))
+        for i in range(self.rows):
+            for j in range(other.cols):
+                for k in range(self.cols):
+                    result.data[i][j] += self.data[i][k] * other.data[k][j]
+        return result
+    
+    # __str__: returns the string representation of the matrix
+    def __str__(self) -> str:
+        result = ""
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result += str(self.data[i][j]) + " "
+            result += "\n"
+        return result
+    
+    # __len__: returns a list of the dimensions of the matrix
+    def __len__(self) -> list:
+        return [self.rows, self.cols]
+    
+    ### append_row, appends a row to the matrix
+    # row: the row to append
+    def append_row(self, row: list) -> None:
+        if len(row) != self.cols:
+            return
+        self.data.append(row)
+        self.rows += 1
+    
+    ### append_col, appends a column to the matrix
+    # col: the column to append
+    def append_col(self, col: list) -> None:
+        if len(col) != self.rows:
+            return
+        for i in range(self.rows):
+            self.data[i].append(col[i])
+        self.cols += 1
+    
+    ### remove_row, removes a row from the matrix
+    # index: the index of the row to remove
+    def remove_row(self, index: int) -> None:
+        if index >= self.rows:
+            return
+        self.data.pop(index)
+        self.rows -= 1
+
+    ### remove_col, removes a column from the matrix
+    # index: the index of the column to remove
+    def remove_col(self, index: int) -> None:
+        if index >= self.cols:
+            return
+        for i in range(self.rows):
+            self.data[i].pop(index)
+        self.cols -= 1
+
+
+### dot_product, returns the dot product of two matrices, auto transposing built in
+# mat1: the first matrix
+# mat2: the second matrix
+def dot_product(mat1: matrix, mat2: matrix) -> matrix:
+    if len(mat1[0]) != len(mat2):
+        if len(mat1) != len(mat2[0]):
+            return None
+        else:
+            mat2.transpose()
+    result = matrix(len(mat1), len(mat2[0]))
---
+++ snn.py
@@ -0,0 +1,18 @@
+"""
+Saksham's Custom Neural Network Implementation, will be used for creating the Tic Tac Toe AI
+The AIs will play against each other and learn from their mistakes, no data collection, just play!
+not sure if this will work, but its worth a try!
+
+Author: Saksham Goel
+Date: Feb 15, 2025
+Version: 1.0
+
+Github: @SakshamG7
+Organization: AceIQ
+Website: https://aceiq.ca
+Contact: mail@aceiq.ca
+Location: Canada, ON, Oakville
+"""
+
+# Required Libraries
+import 
